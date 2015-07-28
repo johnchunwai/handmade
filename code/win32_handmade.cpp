0d891d829526aa302886fdb7517979d0c08e53d1
Wai <johnchunwai@gmail.com>
2015-07-28 02:10:47 -0700

    update .gitignore with emacs junk; update shell to set home for emacs and git; update tab to 4 spaces

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -2,11 +2,11 @@
 #include <windows.h>
 
 int CALLBACK wWinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPWSTR lpCmdLine,
-	int nCmdShow
-	)
+                      HINSTANCE hInstance,
+                      HINSTANCE hPrevInstance,
+                      LPWSTR lpCmdLine,
+                      int nCmdShow
+                      )
 {
     MessageBox(NULL, L"hello world", L"hello", MB_OK | MB_ICONINFORMATION);
     return 0;
--- misc/shell.bat
+++ misc/shell.bat
@@ -7,4 +7,6 @@ REM
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set path=w:\handmade\misc;%path%
+REM setting home for emacs to find the .emacs file, also for git to store global config
+set home=w:\home
 cd w:\handmade\code
