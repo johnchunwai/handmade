a45abdeba34a45805dcaba9a06c9e35256062fab
Wai <johnchunwai@gmail.com>
2015-09-22 02:55:50 -0700

    basic windows loop; build.bat now does cmake

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,20 @@
 @echo off
 
+if %1.==. goto No1
+
 mkdir ..\..\build
 pushd ..\..\build
-cl /Zi /D "_UNICODE" /D "UNICODE" ..\handmade\code\win32_handmade.cpp user32.lib
+rem cl /Zi /D "_UNICODE" /D "UNICODE" ..\handmade\code\win32_handmade.cpp user32.lib
+if "%2" == "-g" (
+   cmake -G "Visual Studio 14 2015 Win64" ..\handmade
+)
+cmake --build . --config %1
 popd
+
+goto End1
+
+:No1
+    echo "Usage: build Release|Debug|RelWithDebInfo|MinSizeRel [-g]"
+goto End1
+
+:End1
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,13 +1,128 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+#include <cassert>
+
+void DebugPrintLastError()
+{
+    wchar_t* msg {0};
+    FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                   FORMAT_MESSAGE_FROM_SYSTEM |
+                   FORMAT_MESSAGE_IGNORE_INSERTS,
+                   NULL,
+                   GetLastError(),
+                   MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                   (LPWSTR)&msg,
+                   0,
+                   NULL);
+    OutputDebugStringW(msg);
+    LocalFree(msg);
+}
+
+LRESULT CALLBACK wndProc(
+    _In_ HWND   hwnd,
+    _In_ UINT   msg,
+    _In_ WPARAM wparam,
+    _In_ LPARAM lparam)
+{
+    LRESULT lresult = 0;
+            
+    switch (msg)
+    {
+    case WM_CREATE:
+        OutputDebugStringA("WM_CREATE\n");
+        break;
+    case WM_DESTROY:
+        OutputDebugStringA("WM_DESTROY\n");
+        PostQuitMessage(0);
+        break;
+    case WM_CLOSE:
+        OutputDebugStringA("WM_CLOSE\n");
+        DestroyWindow(hwnd);
+        break;
+    case WM_NCDESTROY:
+        OutputDebugStringA("WM_NCDESTROY\n");
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT paint;
+            HDC dvcCtx = BeginPaint(hwnd, &paint);
+            int x = paint.rcPaint.left;
+            int y = paint.rcPaint.top;
+            int w = paint.rcPaint.right - x;
+            int h = paint.rcPaint.bottom - y;
+            static DWORD op = WHITENESS;
+            PatBlt(dvcCtx, x, y, w, h, op);
+            if (op == WHITENESS)
+            {
+                op = BLACKNESS;
+            }
+            else
+            {
+                op = WHITENESS;
+            }
+            EndPaint(hwnd, &paint);
+        }
+        break;
+    default:
+        lresult = DefWindowProcW(hwnd, msg, wparam, lparam);
+        break;
+    }
+    return lresult;
+}
+
+
 int CALLBACK wWinMain(
     HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPWSTR lpCmdLine,
-    int nCmdShow
-                      )
+    HINSTANCE,  // hPrevInst is useless
+    LPWSTR,     // not using lpCmdLine
+    int)        // not using nCmdShow
 {
-    MessageBox(NULL, L"hello world", L"hello", MB_OK | MB_ICONINFORMATION);
-    return 0;
+    wchar_t* wndClassName = L"Handmade Hero Window Class";
+    WNDCLASSEXW wndClass = {};  // c++11 aggregate initialization to zero the struct
+    wndClass.cbSize = sizeof(WNDCLASSEX);
+    // wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wndClass.lpfnWndProc = wndProc;
+    wndClass.hInstance = hInstance;
+    wndClass.lpszClassName = wndClassName;
+
+    ATOM wndClsAtom = RegisterClassExW(&wndClass);
+    if (0 == wndClsAtom)
+    {
+        DebugPrintLastError();
+        assert(0 != wndClsAtom);
+        return 0;
+    }
+
+    HWND hwnd = CreateWindowExW(0, wndClassName, L"Handmade Hero", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                                CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                                NULL, NULL, hInstance, NULL);
+    if (hwnd == NULL)
+    {
+        DebugPrintLastError();
+        assert(NULL != hwnd);
+        return 0;
+    }
+
+    // message pump
+    MSG msg;
+    for (;;)  // same as while (true) but with compiler warning for cond exp always const
+    {
+        BOOL getMsgResult = GetMessageW(&msg, NULL, 0, 0);
+        if (getMsgResult == 0)
+        {
+            break;
+        }
+        if (getMsgResult == -1)
+        {
+            DebugPrintLastError();
+            assert(-1 != getMsgResult);
+            return -1;
+        }
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+    
+    // MessageBoxW(NULL, L"hello world", L"hello", MB_OK | MB_ICONINFORMATION);
+    return (int)msg.wParam;  // cast is needed for 64bit app or windows warns we're downcasting
 }
