88bac265039b137c51b58890c610504690e67f4c
Wai <johnchunwai@gmail.com>
2015-10-02 03:03:42 -0700

    handmade day 5.5

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -2,57 +2,126 @@
 #include <windows.h>
 
 #include <cassert>
+#include <cstdint>
 
 #define local_persist static
 #define global_variable static
 #define internal static
 
 
-global_variable bool gRunning = false;
+struct Win32OffscreenBuffer
+{
+    BITMAPINFO info;
+    void *memory;
+    int width;
+    int height;
+    int bytesPerPixel;
+    ptrdiff_t pitch;
+};
 
-global_variable BITMAPINFO bitmapInfo{};
-global_variable void *bitmapMemory = NULL;
+global_variable bool gRunning = false;
+global_variable Win32OffscreenBuffer gBackbuffer {};
+
+struct Win32WindowDimension
+{
+    int width;
+    int height;
+};
+
+internal Win32WindowDimension Win32GetWindowDimension(HWND hwnd)
+{
+    Win32WindowDimension result {};
+    RECT windowRect;
+    // rc.top and rc.left are always 0
+    GetClientRect(hwnd, &windowRect);
+    result.width = windowRect.right - windowRect.left;
+    result.height = windowRect.bottom - windowRect.top;
+    return result;
+}
 
 internal void Win32DebugPrintLastError()
 {
-    wchar_t *msg = NULL;
+    wchar_t *msg = nullptr;
     FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER |
                    FORMAT_MESSAGE_FROM_SYSTEM |
                    FORMAT_MESSAGE_IGNORE_INSERTS,
-                   NULL,
+                   nullptr,
                    GetLastError(),
                    MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
                    (LPWSTR)&msg,
                    0,
-                   NULL);
+                   nullptr);
     OutputDebugStringW(msg);
     LocalFree(msg);
 }
 
-internal void Win32ResizeDibSection(int width, int height)
+internal void RenderWeirdGradient(Win32OffscreenBuffer buffer, int xoffset, int yoffset)
+{
+    // draw something
+    uint8_t *row = static_cast<uint8_t*>(buffer.memory);
+    for (int y = 0; y < buffer.height; ++y)
+    {
+        uint32_t* pixel = reinterpret_cast<uint32_t*>(row);
+        for (int x = 0; x < buffer.width; ++x)
+        {
+            /*
+              pixel in memory:
+              BITMAPINFOHEADER's biBitCount mentions that order is BB GG RR XX.
+
+              Since we're in little endian (least sig byte in lower addr),
+              32bit int representation is 0xXXRRGGBB
+
+              Memory:    BB GG RR xx
+              Register:  xx RR GG BB
+            */
+            uint8_t red = 0;
+            uint8_t green = y + yoffset;
+            uint8_t blue = x + xoffset;
+            *pixel++ = (red << 16) | (green << 8) | blue;
+        }
+        row += buffer.pitch;
+    }
+}
+
+internal void Win32ResizeDibSection(Win32OffscreenBuffer &buffer, int width, int height)
 {
     // TODO: bulletproof this.
     // maybe don't free first, free after.
+    if (buffer.memory)
+    {
+        VirtualFree(buffer.memory, 0, MEM_RELEASE);
+        buffer.memory = nullptr;
+    }
 
-    bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bitmapInfo.bmiHeader.biPlanes = 1;
-    bitmapInfo.bmiHeader.biBitCount = 32;  // no alpha, the 8-bit padding is for alignment
-    bitmapInfo.bmiHeader.biCompression = BI_RGB;
+    buffer.width = width;
+    buffer.height = height;
 
-    bitmapInfo.bmiHeader.biWidth = width;
-    bitmapInfo.bmiHeader.biHeight = -height;  // negative indicates top down DIB
+    buffer.info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    buffer.info.bmiHeader.biPlanes = 1;
+    buffer.info.bmiHeader.biBitCount = 32;  // no alpha, the 8-bit padding is for alignment
+    buffer.info.bmiHeader.biCompression = BI_RGB;
+    buffer.info.bmiHeader.biWidth = buffer.width;
+    buffer.info.bmiHeader.biHeight = -buffer.height;  // negative indicates top down DIB
+    
+    buffer.bytesPerPixel = buffer.info.bmiHeader.biBitCount / 8;
+    buffer.pitch = buffer.width * buffer.bytesPerPixel;
 
     // StretchDIBits doesn't need device context or DibSection (compared to BitBlt)
     // just need the memory to be aligned on DWORD boundary
-    bitmapMemory = ;
+    int bitmapMemorySize = buffer.width * buffer.height * buffer.bytesPerPixel;
+    buffer.memory = VirtualAlloc(nullptr, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+    assert(buffer.memory);
 }
 
-internal void Win32UpdateWindow(HDC dvcCtx, int x, int y, int w, int h)
+internal void Win32DisplayOffscreenBuffer(Win32OffscreenBuffer buffer,
+                                          HDC dvcCtx, int width, int height)
 {
+    // Setting to stretch blt mode to prevent artifacts when shrink blt.
+    SetStretchBltMode(dvcCtx, STRETCH_DELETESCANS);
     int stretchResult = StretchDIBits(dvcCtx,
-                                      x, y, w, h,
-                                      x, y, w, h,
-                                      bitmapMemory, &bitmapInfo,
+                                      0, 0, width, height,
+                                      0, 0, buffer.width, buffer.height,
+                                      buffer.memory, &buffer.info,
                                       DIB_RGB_COLORS, SRCCOPY);
 }
 
@@ -76,7 +145,7 @@ LRESULT CALLBACK Win32WndProc(
             // int height = rc.bottom;
             int width = LOWORD(lparam);
             int height = HIWORD(lparam);
-            Win32ResizeDibSection(width, height);
+            // Win32ResizeDibSection(gBackbuffer, width, height);
         }
         break;
 
@@ -104,11 +173,13 @@ LRESULT CALLBACK Win32WndProc(
         {
             PAINTSTRUCT paint;
             HDC dvcCtx = BeginPaint(hwnd, &paint);
-            int x = paint.rcPaint.left;
-            int y = paint.rcPaint.top;
-            int w = paint.rcPaint.right - x;
-            int h = paint.rcPaint.bottom - y;
-            Win32UpdateWindow(dvcCtx, x, y, w, h);
+            // int x = paint.rcPaint.left;
+            // int y = paint.rcPaint.top;
+            // int w = paint.rcPaint.right - x;
+            // int h = paint.rcPaint.bottom - y;
+
+            Win32WindowDimension dimension = Win32GetWindowDimension(hwnd);
+            Win32DisplayOffscreenBuffer(gBackbuffer, dvcCtx, dimension.width, dimension.height);
             EndPaint(hwnd, &paint);
         }
         break;
@@ -127,10 +198,11 @@ int CALLBACK wWinMain(
     LPWSTR,     // not using lpCmdLine
     int)        // not using nCmdShow
 {
+    Win32ResizeDibSection(gBackbuffer, 1280, 720);
     wchar_t *wndClassName = L"Handmade Hero Window Class";
     WNDCLASSEXW wndClass = {};  // c++11 aggregate initialization to zero the struct
     wndClass.cbSize = sizeof(WNDCLASSEX);
-    // wndClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;  // | CS_OWNDC
     wndClass.lpfnWndProc = Win32WndProc;
     wndClass.hInstance = hInstance;
     wndClass.lpszClassName = wndClassName;
@@ -145,40 +217,48 @@ int CALLBACK wWinMain(
 
     HWND hwnd = CreateWindowExW(0, wndClassName, L"Handmade Hero", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                                 CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-                                NULL, NULL, hInstance, NULL);
-    if (hwnd == NULL)
+                                nullptr, nullptr, hInstance, nullptr);
+    if (hwnd == nullptr)
     {
         Win32DebugPrintLastError();
-        assert(NULL != hwnd);
+        assert(nullptr != hwnd);
         return 0;
     }
 
     // message pump
     gRunning = true;
+    int xoffset = 0;
+    int yoffset = 0;
     while (gRunning)
     {
         MSG msg;
-        BOOL msgResult = GetMessageW(&msg, NULL, 0, 0);
-        if (msgResult > 0)
+        // BOOL msgResult = GetMessageW(&msg, nullptr, 0, 0);
+        while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+        // if (msgResult > 0)
         {
+            if (msg.message == WM_QUIT)
+            {
+                gRunning = false;
+                break;
+            }
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
         }
-        else
+
+        if (!gRunning)
         {
-            if (msgResult == 0)
-            {
-                break;
-            }
-            if (msgResult == -1)
-            {
-                Win32DebugPrintLastError();
-                assert(-1 != msgResult);
-                return -1;
-            }
+            break;
         }
+
+        RenderWeirdGradient(gBackbuffer, xoffset, yoffset);
+        HDC dvcCtx = GetDC(hwnd);
+        Win32WindowDimension dimension = Win32GetWindowDimension(hwnd);
+        Win32DisplayOffscreenBuffer(gBackbuffer, dvcCtx, dimension.width, dimension.height);
+        ReleaseDC(hwnd, dvcCtx);
+        ++xoffset;
+        yoffset += 2;
     }
     
-    // MessageBoxW(NULL, L"hello world", L"hello", MB_OK | MB_ICONINFORMATION);
+    // MessageBoxW(nullptr, L"hello world", L"hello", MB_OK | MB_ICONINFORMATION);
     return 0;
 }
