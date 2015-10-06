d74d2ba6ef598fbec5edd623a795952e06f1cb0b
Wai <johnchunwai@gmail.com>
2015-10-06 02:25:14 -0700

    day 6

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,5 +1,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <xinput.h>
 
 #include <cassert>
 #include <cstdint>
@@ -18,14 +19,57 @@ struct Win32OffscreenBuffer
     ptrdiff_t pitch;
 };
 
-global_variable bool gRunning = false;
-global_variable Win32OffscreenBuffer gBackbuffer {};
-
 struct Win32WindowDimension
 {
     int width;
     int height;
 };
+
+// TODO: global for now
+global_variable bool gRunning = false;
+global_variable Win32OffscreenBuffer gBackbuffer {};
+
+// XInputGetState
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE* pState)
+typedef X_INPUT_GET_STATE(XInputGetStateTypedef);
+X_INPUT_GET_STATE(XInputGetStateStub)
+{
+    return ERROR_DEVICE_NOT_CONNECTED;
+}
+global_variable XInputGetStateTypedef *XInputGetState_ = XInputGetStateStub;
+#define XInputGetState XInputGetState_
+
+// XInputSetState
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION* pVibration)
+typedef X_INPUT_SET_STATE(XInputSetStateTypedef);
+X_INPUT_SET_STATE(XInputSetStateStub)
+{
+    return ERROR_DEVICE_NOT_CONNECTED;
+}
+global_variable XInputSetStateTypedef *XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
+
+internal void Win32LoadXInput()
+{
+    char *xInputDlls[] = {
+        "xinput1_4.dll",
+        "xinput1_3.dll",
+        "xinput9_1_0.dll"
+    };
+    for (auto &xInputDll : xInputDlls)
+    {
+        HMODULE xInputLibrary = LoadLibraryA (xInputDll);
+        if (xInputLibrary)
+        {
+            XInputGetState = reinterpret_cast<XInputGetStateTypedef*>(GetProcAddress(xInputLibrary,
+                                                                                     "XInputGetState"));
+            XInputSetState = reinterpret_cast<XInputSetStateTypedef*>(GetProcAddress(xInputLibrary,
+                                                                                     "XInputSetState"));
+            assert(XInputGetState && XInputSetState);
+            break;
+        }
+    }
+}
 
 internal Win32WindowDimension Win32GetWindowDimension(HWND hwnd)
 {
@@ -52,7 +96,7 @@ internal void Win32DebugPrintLastError()
     LocalFree(msg);
 }
 
-internal void RenderWeirdGradient(Win32OffscreenBuffer buffer, int blueOffset, int greenOffset)
+internal void RenderWeirdGradient(Win32OffscreenBuffer &buffer, int blueOffset, int greenOffset)
 {
     // draw something
     uint8_t *row = static_cast<uint8_t*>(buffer.memory);
@@ -111,7 +155,7 @@ internal void Win32ResizeBackBuffer(Win32OffscreenBuffer &buffer, int width, int
     assert(buffer.memory);
 }
 
-internal void Win32DisplayOffscreenBuffer(Win32OffscreenBuffer buffer,
+internal void Win32DisplayOffscreenBuffer(Win32OffscreenBuffer &buffer,
                                           HDC dvcCtx, int width, int height)
 {
     // Setting to stretch blt mode to prevent artifacts when shrink blt.
@@ -123,7 +167,7 @@ internal void Win32DisplayOffscreenBuffer(Win32OffscreenBuffer buffer,
                                       DIB_RGB_COLORS, SRCCOPY);
 }
 
-LRESULT CALLBACK Win32WndProc(
+internal LRESULT CALLBACK Win32WndProc(
     _In_ HWND   hwnd,
     _In_ UINT   msg,
     _In_ WPARAM wparam,
@@ -160,6 +204,97 @@ LRESULT CALLBACK Win32WndProc(
         }
         break;
 
+    case WM_SYSKEYDOWN:
+    case WM_SYSKEYUP:
+    case WM_KEYDOWN:
+    case WM_KEYUP:
+        {
+            WPARAM vkCode = wparam;
+            bool wasDown = ((lparam & (1 << 30)) != 0);
+            bool isDown = ((lparam & (1 << 31)) == 0);
+            if (isDown != wasDown)
+            {
+                switch (vkCode)
+                {
+                case 'W':
+                    {
+                        OutputDebugStringA("W\n");
+                    }
+                    break;
+                case 'A':
+                    {
+                        OutputDebugStringA("A\n");
+                    }
+                    break;
+                case 'S':
+                    {
+                        OutputDebugStringA("S\n");
+                    }
+                    break;
+                case 'D':
+                    {
+                        OutputDebugStringA("D\n");
+                    }
+                    break;
+                case 'Q':
+                    {
+                        OutputDebugStringA("Q\n");
+                    }
+                    break;
+                case 'E':
+                    {
+                        OutputDebugStringA("E\n");
+                    }
+                    break;
+                case VK_UP:
+                    {
+                        OutputDebugStringA("UP\n");
+                    }
+                    break;
+                case VK_DOWN:
+                    {
+                        OutputDebugStringA("DOWN\n");
+                    }
+                    break;
+                case VK_LEFT:
+                    {
+                        OutputDebugStringA("LEFT\n");
+                    }
+                    break;
+                case VK_RIGHT:
+                    {
+                        OutputDebugStringA("RIGHT\n");
+                    }
+                    break;
+                case VK_ESCAPE:
+                    {
+                        OutputDebugStringA("ESCAPE: ");
+                        if (isDown)
+                        {
+                            OutputDebugStringA("isDown ");
+                        }
+                        if (wasDown)
+                        {
+                            OutputDebugStringA("wasDown ");
+                        }
+                        OutputDebugStringA("\n");
+                    }
+                    break;
+                case VK_SPACE:
+                    {
+                        OutputDebugStringA("SPACE\n");
+                    }
+                    break;
+                case VK_RETURN:
+                    {
+                        OutputDebugStringA("ENTER\n");
+                    }
+                    break;
+                }
+            }
+        }
+        break;
+
     case WM_PAINT:
         {
             PAINTSTRUCT paint;
@@ -187,6 +322,7 @@ int CALLBACK wWinMain(
     int)        // not using nCmdShow
 {
     Win32ResizeBackBuffer(gBackbuffer, 1280, 720);
+    Win32LoadXInput();
     wchar_t *wndClassName = L"Handmade Hero Window Class";
     WNDCLASSEXW wndClass = {};  // c++11 aggregate initialization to zero the struct
     wndClass.cbSize = sizeof(WNDCLASSEX);
@@ -236,15 +372,68 @@ int CALLBACK wWinMain(
         if (!gRunning)
         {
             break;
-         }
+        }
+
+        for (DWORD controllerIndex = 0; controllerIndex< XUSER_MAX_COUNT; ++controllerIndex)
+        {
+            XINPUT_STATE controllerState {};
+            // Simply get the state of the controller from XInput.
+            if (ERROR_SUCCESS == XInputGetState(controllerIndex, &controllerState))
+            {
+                // Controller is connected
+                // dwPacketNumber indicates whether there have been state changes
+                controllerState.dwPacketNumber;
+                XINPUT_GAMEPAD &pad = controllerState.Gamepad;
+                // these are what we care about
+                bool up = pad.wButtons & XINPUT_GAMEPAD_DPAD_UP;
+                bool down = pad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+                bool left = pad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+                bool right = pad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+                bool start = pad.wButtons & XINPUT_GAMEPAD_START;
+                bool back = pad.wButtons & XINPUT_GAMEPAD_BACK;
+                bool leftShoulder = pad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
+                bool rightShoulder = pad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;
+                bool aButton = pad.wButtons & XINPUT_GAMEPAD_A;
+                bool bButton = pad.wButtons & XINPUT_GAMEPAD_B;
+                bool xButton = pad.wButtons & XINPUT_GAMEPAD_X;
+                bool yButton = pad.wButtons & XINPUT_GAMEPAD_Y;
+
+                int16_t lStickX = pad.sThumbLX;
+                int16_t lStickY = pad.sThumbLY;
+
+                if (xButton)
+                {
+                    blueOffset += 5;
+
+                    // Add some vibration left motor
+                    XINPUT_VIBRATION vibration {};
+                    vibration.wLeftMotorSpeed = 65535;
+                    XInputSetState(controllerIndex, &vibration);
+                }
+                
+                if (yButton)
+                {
+                    greenOffset += 2;
+
+                    // Add some vibration right motor
+                    XINPUT_VIBRATION vibration {};
+                    vibration.wRightMotorSpeed = 10000;
+                    XInputSetState(controllerIndex, &vibration);
+                }
+            }
+            else
+            {
+                // Controller is not connected 
+            }
+        }
 
         RenderWeirdGradient(gBackbuffer, blueOffset, greenOffset);
         HDC dvcCtx = GetDC(hwnd);
         Win32WindowDimension dimension = Win32GetWindowDimension(hwnd);
         Win32DisplayOffscreenBuffer(gBackbuffer, dvcCtx, dimension.width, dimension.height);
         ReleaseDC(hwnd, dvcCtx);
-        ++blueOffset;
-        greenOffset += 2;
+
+        blueOffset++;
     }
     
     // MessageBoxW(nullptr, L"hello world", L"hello", MB_OK | MB_ICONINFORMATION);
